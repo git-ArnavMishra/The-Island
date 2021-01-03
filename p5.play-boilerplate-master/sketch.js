11271727f8efda3fd00d3c176c9e868c5b5088d5
git-Arnav Mishra <arnavfire123@gmail.com>
2021-01-02 21:31:04 -0800

    add health bar

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -1,9 +1,11 @@
 var topImage;
+var man;
 var gameState = 0;
 
 function setup() {
   createCanvas(displayWidth-200,displayHeight-240);
   createSprite(400, 200, 50, 50);
+  man = new Man();
 }
 function preload(){
   topImage = loadImage("Images/Islandasdf.jpg")
@@ -13,6 +15,7 @@ function draw() {
   if(gameState===0){
     textSize(100);
     text("The Island",200,200);
+    man.display();
   }
   else if(gameState===1){
 
